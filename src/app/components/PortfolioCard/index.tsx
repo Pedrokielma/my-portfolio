26aeb84ed7c62093bac7059f0d3ca6740697ecf4
pedrokielma <peterkielma@gmail.com>
2023-06-26 21:39:40 +0100

    WIP portfolio carrosel

--- src/app/components/PortfolioCard/index.tsx
+++ src/app/components/PortfolioCard/index.tsx
@@ -1,38 +1,55 @@
 import style from "./portfolioCard.module.scss";
-// import { useState } from 'react';
+import { useState, useEffect } from "react";
 import classNames from "classnames/bind";
 import RoundButton from "@/app/components/RoundButton/index";
+import { useInView } from "react-intersection-observer";
 const cx = classNames.bind(style);
 
-interface FooProp {
+interface Prop {
   name: String;
   html_url: string;
   description: string;
-  index: number
+  index: number;
+  cardInView: CardInView[];
+  setCardInView: React.Dispatch<React.SetStateAction<CardInView[]>>;
 }
 
 
-const PortfolioCard = (props: FooProp) => {
-  const { name, html_url, description, index } = props;
+interface CardInView {
+  [key: number]: boolean;
+}
+
+const PortfolioCard = (props: Prop) => {
+  const { name, html_url, description, index, setCardInView, cardInView } =
+    props;
+
+  const { ref, inView } = useInView({
+    threshold: 0.5,
+  });
+
+ 
+
+  useEffect(() => {
+    setCardInView((prevCardInView) => {
+        const updatedCardInView = [...prevCardInView];
+        updatedCardInView[index] = { [index]: inView };
+        return updatedCardInView;
+      });
+
+  }, [inView]);
+
+  
   return (
-    <div className={style.itemCard}>
-    <a
-      href={html_url}
-      className={style.item}
-      target="_blank"
-    >
-      <p className={style.number}>
-        {String(index + 1).padStart(2, "0")}
-      </p>
-      <p className={style.name}>
-        {name.replace("_", "-")}
-      </p>
-      <p className={style.description}>{description}</p>
-      <div className={style.roundButton}>
-        <RoundButton size="small" content="VIEW" />
-      </div>
-    </a>
-  </div>
+    <div ref={ref} key={index} className={style.itemCard}>
+      <a href={html_url} className={style.item} target="_blank">
+        <p className={style.number}>{String(index + 1).padStart(2, "0")}</p>
+        <p className={style.name}>{name.replace("_", "-")}</p>
+        <p className={style.description}>{description}</p>
+        <div className={style.roundButton}>
+          <RoundButton size="small" content="VIEW" />
+        </div>
+      </a>
+    </div>
   );
 };
 
