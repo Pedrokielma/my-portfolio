35c9e51ed0afcbdccb54f7e8de1d71a797782e87
pedrokielma <peterkielma@gmail.com>
2023-06-30 18:11:28 +0100

    seo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,26 +1,61 @@
 import "./styles/globals.scss";
 
-
+// export const metadata = {
+//   title: "Pedro Kielma - Frontend Developer Portfolio",
+//   description:
+//     "I am a frontend developer experienced in Next.js, TypeScript, JavaScript, React, Node.js, and Express. Check out my portfolio to see my work and skills.",
+//   openGraph: {
+//     type: "website",
+//     locale: "en_US",
+//     url: "https://pedrokielma.com",
+//     site_name: "Pedro Kielma Portfolio",
+//     images: [
+//       {
+//         url: "https://pedrokielma.com/images/profile-image.png",
+//         width: 800,
+//         height: 600,
+//         alt: "Pedro Kielma - Frontend Developer",
+//         type: "image/jpeg",
+//       },
+//     ],
+//   },
+// };
 
 export const metadata = {
-  title: "Pedro Kielma Portfolio",
-  description: "created by PedroKielma",
+  title: "Pedro Kielma - Frontend Developer Portfolio",
+  description:
+    "I am a frontend developer experienced in Next.js, TypeScript, JavaScript, React, Node.js, and Express. Check out my portfolio to see my work and skills.",
   openGraph: {
     type: "website",
     locale: "en_US",
     url: "https://pedrokielma.com",
     site_name: "Pedro Kielma Portfolio",
-
     images: [
       {
-        url: "../../public/images/profile-image.png",
+        url: "https://pedrokielma.com/images/profile-image.png",
         width: 800,
         height: 600,
-        alt: "Og Image Alt",
+        alt: "Pedro Kielma - Frontend Developer",
         type: "image/jpeg",
       },
     ],
   },
+  additionalMetaTags: [
+    {
+      property: "image",
+      content: "https://pedrokielma.com/images/profile-image.png",
+    },
+    {
+      property: "og:title",
+      content: "Pedro Kielma - Frontend Developer Portfolio",
+    },
+    { name: "apple-mobile-web-app-title", content: "Pedro Kielma Portfolio" },
+    {
+      rel: "apple-touch-startup-image",
+      href: "https://pedrokielma.com/images/profile-image.png",
+    },
+  ],
+  additionalLinkTags: [{ rel: "shortcut icon", href: "/favicon.ico" }],
 };
 
 export default function RootLayout({
@@ -29,10 +64,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-      <html lang="en">
-        <body>{children}</body>
-      </html>
- 
+    <html lang="en">
+      <body>{children}</body>
+    </html>
   );
 }
-
