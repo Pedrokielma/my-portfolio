3b5e0fd8efdff53431015eaf6389fdd005ed0f57
pedrokielma <peterkielma@gmail.com>
2023-06-30 15:44:58 +0100

    seo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,11 +3,11 @@ import "./styles/globals.scss";
 import SEOConfig from "./next-seo.config";
 import { DefaultSeo } from "next-seo";
 
-export const metadata = {
-  title: "Pedro Kielma Portfolio",
-  description: "created by PedroKielma",
-  imageDescription: "Description of your image",
-};
+// export const metadata = {
+//   title: "Pedro Kielma Portfolio",
+//   description: "created by PedroKielma",
+//   imageDescription: "Description of your image",
+// };
 
 export default function RootLayout({
   children,
