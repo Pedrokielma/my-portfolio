b65deec615d5cf23d1b54f56cd4fdfdca9abe12f
pedrokielma <peterkielma@gmail.com>
2023-07-01 12:42:05 +0100

    404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,27 +1,43 @@
-
 // import { FiArrowLeft } from "react-icons/fi";
+"use client";
+import { useRouter } from "next/navigation";
+
 import style from "./styles/not-found.module.scss";
 
 export default function NotFound() {
+  const router = useRouter();
 
-  
-    return (
-      <div className={style.notFound}>
-        <div className={style.notFoundSection}>
-        <p className={style.errorName}>404 error</p>
-        <p className={style.notFoundTitle}>Page not found</p>
-        <p className={style.notFoundText}>Sorry, the page you are looking for doesn`t exist. <br />Here are some helpful links:</p>
-        <div>
-          {/* <a className={style.notFoundText}> 
+  const handleHomePage = () => {
+    router.push("/");
+  };
+
+  return (
+    <div className={style.notFound}>
+      <div className={style.notFoundSection}>
+        <div className={style.notFoundContainer}>
+          <p className={style.errorName}>404 error</p>
+          <p className={style.notFoundTitle}>Page not found</p>
+          <p className={style.notFoundText}>
+            Sorry, the page you are looking for doesn`t exist. <br />
+            Here are some helpful links:
+          </p>
+          <div className={style.buttonSection}>
+            {/* <a className={style.notFoundText}> 
           <FiArrowLeft />
           <span>Go back</span> 
           </a> */}
-          <a className={style.buttonHome}>Take me home</a>
+            <a
+              className={style.buttonHome}
+              onClick={() => {
+                handleHomePage();
+              }}
+            >
+              Take me home
+            </a>
+          </div>
         </div>
-
-
-        </div>
-        <div className={style.notFoundImage}/>
       </div>
-    );
-  }
+      <div className={style.notFoundImage} />
+    </div>
+  );
+}
